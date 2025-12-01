385b893e724cfaf66be2c7b372122b0ec74c35bf
Phạm Nhật Trường <ad@truongit.net>
2025-12-01 21:06:00 +0700

    Update privacy URL in Install and Settings pages to point to Helpdesk site

--- flutter/lib/mobile/pages/settings_page.dart
+++ flutter/lib/mobile/pages/settings_page.dart
@@ -928,7 +928,7 @@ class _SettingsState extends State<SettingsPage> with WidgetsBindingObserver {
             SettingsTile(
               title: Text(translate("Privacy Statement")),
               onPressed: (context) =>
-                  launchUrlString('https://rustdesk.com/privacy.html'),
+                  launchUrlString('https://helpdesk.truongit.net/privacy.html'),
               leading: Icon(Icons.privacy_tip),
             )
           ],
